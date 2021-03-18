992323093dd1fad3c9fe7eb9479b2d78d9684176
Surya <skandru@my.centennialcollege.ca>
2021-03-18 02:50:32 -0400

    deleted

--- app/config/db.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-
-    URI : "mongodb+srv://root:roor123@cluster0.xpr8p.mongodb.net/CarPooling?retryWrites=true&w=majority"
-   
-   }
--- app/controllers/index.server.controller.js
+++
@@ -1,9 +0,0 @@
-
-exports.displayInfo=function(req,res){
-    var username=req.body.username;
-    var session=req.session;
-    session.username=username;console.log("usernameinsession:"+session.username);
-
-res.render('display',
-{username:username});
-};
