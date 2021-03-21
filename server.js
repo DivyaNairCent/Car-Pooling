1c9e53942867b11b98eca710a4f4f163e2d39fb9
singhjp006 <jashanpreet@khalsa.com>
2021-03-21 23:54:07 +0530

    added team logo, fixed minor bug, added commments

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
+// server.js
+// CarPooling Web App
+// Project by Team CodEureka
+// Silviya Velani, Student Id: 301167163
+// Divya Nair, Student Id: 301169854 
+// Jashan Preet Singh, Student ID: 301170664
+// Surya Teja Kandru, Student Id: 301109137 
+// Aritra Roy, Student ID: 301176508 
+// Copyright © 2021 Centennial College. All rights reserved.
+
 /**
  * Module dependencies.
  */
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,3 +1,14 @@
+// user.js
+// CarPooling Web App
+// Project by Team CodEureka
+// Silviya Velani, Student Id: 301167163
+// Divya Nair, Student Id: 301169854 
+// Jashan Preet Singh, Student ID: 301170664
+// Surya Teja Kandru, Student Id: 301109137 
+// Aritra Roy, Student ID: 301176508 
+// Copyright © 2021 Centennial College. All rights reserved.
+
+
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
