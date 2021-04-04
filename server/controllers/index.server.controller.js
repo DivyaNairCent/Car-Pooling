33defbfa29ef8b8284aea4acfeea4e5e274afefe
Silviya Velani <svelani1@my.centennialcollege.ca>
2021-04-04 10:27:30 -0400

    nav bar set up and my rides list

--- server/controllers/index.server.controller.js
+++ server/controllers/index.server.controller.js
@@ -10,11 +10,11 @@
 
 
 module.exports.displayHomePage = (req, res, next) => {
-    res.render('index', {title: 'Home'});
+    res.render('index', {title: 'Home', displayName: req.user ? req.user.displayName : ''});
 }
 
 module.exports.displayAboutPage = (req, res, next) => {
-    res.render('index', {title: 'About'});
+    res.render('index', {title: 'About', displayName: req.user ? req.user.displayName : ''});
 }
 
 // module.exports.displayAddRidePage = (req, res, next) => {
@@ -26,7 +26,7 @@ module.exports.displayAboutPage = (req, res, next) => {
 // }
 
 module.exports.displayContactPage = (req, res, next) => {
-    res.render('index', {title: 'Contact'});
+    res.render('index', {title: 'Contact', displayName: req.user ? req.user.displayName : ''});
 }
 
 
